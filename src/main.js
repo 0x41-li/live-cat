6db07bbbc1dac94ddbf6a28fd918ebd53c09262d
Ali Sbane <ali.sbane.dev@gmail.com>
2024-05-13 02:04:27 +0100

    Added the Twitch live to the site

--- src/main.js
+++ src/main.js
@@ -6,7 +6,14 @@ import "aos/dist/aos.css";
 import "./index.css";
 
 AOS.init({
-  once: true,
   startEvent: "load",
   easing: "ease-in-out",
 });
+
+var options = {
+  channel: "livecatsol",
+  width: "100%",
+  height: "100%",
+};
+var player = new Twitch.Player("twitch-live", options);
+player.setVolume(0.5);
